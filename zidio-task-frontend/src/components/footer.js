5c6be608cd80fa5aa1db7f9e6a1dfe9bb52a14c4
Gayatrisawant19 <152778039+Gayatrisawant19@users.noreply.github.com>
2025-01-31 15:10:50 +0530

    Create footer.js

---
+++ zidio-task-frontend/src/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center mt-10">
+      <p>© {new Date().getFullYear()} Zidio Task Management. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
