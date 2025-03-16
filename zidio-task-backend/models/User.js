8277a0a6ee8940c8292e454dc2db86f5593216b1
Tarun <tarunbusinessmail@gmail.com>
2025-03-16 05:17:23 +0000

    Fixing Error 500

--- zidio-task-backend/models/User.js
+++ zidio-task-backend/models/User.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: { type: String, unique: true, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["admin", "user"], default: "user" }, // Admin/User Role
 });
 
 module.exports = mongoose.model("User", userSchema);
